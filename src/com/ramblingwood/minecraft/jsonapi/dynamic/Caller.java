aea403511a57d32d8d65e8ee1b3b6035ec57e61e
Alec Gorge <alecgorge@gmail.com>
2011-04-01 15:29:09 -0400

    it works (except for nested returns)

--- src/com/ramblingwood/minecraft/jsonapi/dynamic/Caller.java
+++ src/com/ramblingwood/minecraft/jsonapi/dynamic/Caller.java
@@ -1,25 +1,30 @@
 package com.ramblingwood.minecraft.jsonapi.dynamic;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.json.simpleForBukkit.JSONArray;
+import org.json.simpleForBukkit.JSONObject;
+import org.json.simpleForBukkit.parser.JSONParser;
+import org.json.simpleForBukkit.parser.ParseException;
 
 public class Caller {
-	public static boolean initted = false;
-	public static HashMap<String, Method> methods;
+	public HashMap<String, Method> methods = new HashMap<String, Method>();
 	
-	public Caller (File i) {
-		initMethods(i);
+	public Caller () {
 	}
 	
 	public Object call(String method, Object[] params) {
 		Object r = null;
 		try {
-			methods.get(method).getCall().call(params);
+			System.out.println("Called:"+method);
+			System.out.println("with args:"+Arrays.asList(params).toString());
+			r = methods.get(method).getCall().call(params);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -28,18 +33,21 @@ public class Caller {
 		return r;
 	}
 	
-	public static void initMethods (File methodsFile) {
+	public void loadFile (File methodsFile) {
+		JSONParser p = new JSONParser();
+		JSONArray methods;
 		try {
-			if(!initted) {			
-				initted = true;
-				
-				JSONParser p = new JSONParser();
-				JSONArray methods = (JSONArray)p.parse(new FileReader(methodsFile));
-				
-				for(Object o : methods) {
-					if(o instanceof JSONObject) {
-						Caller.methods.put(((JSONObject)o).get("name").toString(), new Method((JSONObject)o));
+			methods = (JSONArray)p.parse(new FileReader(methodsFile));
+		
+			for(Object o : methods) {
+				if(o instanceof JSONObject) {
+					String name = ((JSONObject)o).get("name").toString();
+					
+					if(this.methods.containsKey(name)) {
+						Logger.getLogger("Minecraft").warning("[JSONAPI] The method " + name + " already exists! It is being overridden.");
 					}
+					
+					this.methods.put(name, new Method((JSONObject)o));
 				}
 			}
 		} catch (Exception e) {
