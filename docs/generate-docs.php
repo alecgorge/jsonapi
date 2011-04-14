f9f6398d29b901805ab0e40a15f499ef5c372452
Alec Gorge <alecgorge@gmail.com>
2011-04-13 21:53:07 -0400

    new much better docs with namespace support. also updated json files with namespaces

--- docs/generate-docs.php
+++ docs/generate-docs.php
@@ -2,9 +2,10 @@
 
 require '../sdk/php/JSONAPI.php';
 
-$input_file = "../src/methods.json";
-
-$docs = jsonapi_docs(json_decode(file_get_contents($input_file), true));
+$input_files = array_merge(array("../src/methods.json"), glob("../test/plugins/JSONAPI/methods/*.json"));
+foreach($input_files as $input_file) {
+	$docs .= jsonapi_docs(json_decode(file_get_contents($input_file), true));
+}
 
 ?><!DOCTYPE html>
 <html>
@@ -13,33 +14,71 @@ $docs = jsonapi_docs(json_decode(file_get_contents($input_file), true));
 		<link type="text/css" rel="stylesheet" name="stylesheet" href="styles.css" />
 	</head>
 	<body>
-<?php echo $docs; ?>
+	<h1>API Documentation for JSONAPI</h1>
+	<p>This page documents all the methods that ship with JSONAPI. This includes the base method that work as long as JSONAPI is installed and enabled, along with the methods that require another plugin to be installed and enabled for them to work.
+	The methods that require another plugin to be installed simply do not work if that plugin is not installed and enabled.</p>
+	<h2>Available method packages</h2>
+	<ul>
+<?php
+
+foreach($input_files as $input_file) {
+	$json = json_decode(file_get_contents($input_file), true);
+	printf('<li><a href="#package-%s">%s</a> (%s namespace)</li>', rawurlencode($json['name']), $json['name'], (empty($json['namespace']) ? "default" : $json['namespace']));
+}
+echo "</ul>".$docs; ?>
 	</body>
 </html>
 <?php
 
 function jsonapi_docs($input_json) {
-	$format = '<div id="jsonapi-docs" class="doc-wrapper">%s</div>';
-	foreach($input_json as $method) {
-		$r .= jsonapi_docs_method($method);
+	$format = '<h2 id="package-%s">%s</h2><h3>Depends on the following plugins:</h3><ul>%s</ul><h3>Methods</h3><ul>%s</ul><div id="jsonapi-docs" class="doc-wrapper">%s</div>';
+	
+	foreach($input_json['methods'] as $method) {
+		$r .= jsonapi_docs_method($method, $input_json['namespace']);
 	}
-	return sprintf($format, $r);
+	foreach($input_json['depends'] as $dep) {
+		$lis .= "<li>$dep</li>";
+	}
+	
+	$method_html = "";
+	foreach($input_json['methods'] as $method) {
+		$args = "";
+		foreach((array)$method['args'] as $arg) {
+			$args .= " ".$arg[0].",";
+		}
+		$args = ltrim(rtrim($args, ","));
+		
+		$method_html .= sprintf("<li><a href='#%s'>%s %s(%s)</a></li>", 
+								generate_token($method['call']),
+								(empty($input_json['return']) ? "void" : $input_json['return']),
+								(empty($input_json['namespace']) ? "" : $input_json['namespace'].".").$method['name'],
+								$args);
+	}
+	
+	return sprintf($format, rawurlencode($input_json['name']), $input_json['name'], $lis, $method_html, $r);
 }
 
-function jsonapi_docs_method ($method) {
+function name_for_method_json($input_json, $namespace) {
+	return (empty($namespace) ? "" : $namespace.".").$input_json['name'];
+}
+
+function generate_token($call) {
+	return rawurlencode($call);
+}
+
+function jsonapi_docs_method ($method, $namespace) {
 	$format = <<<EOT
-	<div class="method-wrapper">
+	<div class="method-wrapper" id="%s">
 		<ul class="method-info">
 			<li class="method-info-name"><strong>Name: </strong>%s</li>
 			<li class="method-info-desc"><strong>Description: </strong>%s</li>
 			<li class="method-info-args"><strong>Arguments: </strong>%s</li>
 			<li class="method-info-return"><strong>Returns: </strong>%s</li>
 		</ul>
-		<br class="clear" />
 	</div>
 
 EOT;
-	return sprintf($format, $method['name'], $method['desc'], jsonapi_docs_args($method['args']), jsonapi_docs_returns($method['returns']));
+	return sprintf($format, generate_token($method['call']), name_for_method_json($method, $namespace), $method['desc'], jsonapi_docs_args($method['args']), jsonapi_docs_returns($method['returns']));
 }
 
 function jsonapi_docs_args ($args) {
@@ -58,7 +97,7 @@ function jsonapi_docs_args ($args) {
 
 function jsonapi_docs_returns ($returns) {
 	if(empty($returns)) {
-		return "null";
+		return "void";
 	}
 	
 	return sprintf("<strong>%s</strong> %s", $returns[0], $returns[1]);
