02aabef75c64bbda9d90ca3cf0e71eae3dd5439e
Alec Gorge <alecgorge@gmail.com>
2011-05-01 20:39:57 -0400

    examples

---
+++ examples/00-basic.php
@@ -0,0 +1,6 @@
+<?php
+
+require('../sdk/php/JSONAPI.php');
+
+$api = new JSONAPI("localhost", 20059, "admin", "demo", ""); // host/ip, port, username, password, salt
+var_dump($api->call("getPlayerLimit"));
