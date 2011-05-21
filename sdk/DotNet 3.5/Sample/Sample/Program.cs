2ac83dc2cb5ef5227af9d9bdf949715c10e4b493
Alec Gorge <alecgorge@gmail.com>
2011-05-21 11:45:23 -0400

    .net sdk

---
+++ sdk/DotNet 3.5/Sample/Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.ramblingwood.minecraft.jsonapi;
+using System.Collections;
+
+namespace Sample {
+	class Program {
+		static void Main(string[] args) {
+			JSONAPI j = new JSONAPI("localhost", 20059, "admin", "demo", "");
+
+			Dictionary<object, object> result = j.call("getPlayers", new ArrayList());
+			Console.WriteLine(result["result"]);
+
+			foreach(object o in ((ArrayList)result[result["result"]])) {
+				foreach(object key in ((Hashtable)o).Keys) {
+					Console.WriteLine(key + ":"+((Hashtable)o)[key]);
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
