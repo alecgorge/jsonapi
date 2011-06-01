04a0bb72808926763376983e5fed1cdf207d9f92
Alec Gorge <alecgorge@gmail.com>
2011-05-31 21:05:44 -0400

    c# example change

--- sdk/DotNet 3.5/Sample/Sample/Program.cs
+++ sdk/DotNet 3.5/Sample/Sample/Program.cs
@@ -10,14 +10,21 @@ namespace Sample {
 		static void Main(string[] args) {
 			JSONAPI j = new JSONAPI("localhost", 20059, "admin", "demo", "");
 
-			Dictionary<object, object> result = j.call("getPlayers", new ArrayList());
+			ArrayList arr = new ArrayList();
+			arr.Add("alefcgorge");
+			arr.Add("test");
+			Dictionary<object, object> result = j.call("broadcastWithName", arr);
+			Console.ReadLine();
 			Console.WriteLine(result["result"]);
 
-			foreach(object o in ((ArrayList)result[result["result"]])) {
-				foreach(object key in ((Hashtable)o).Keys) {
-					Console.WriteLine(key + ":"+((Hashtable)o)[key]);
+			if (result[result["result"]] != null && result[result["result"]].ToString() != "null") {
+				foreach (object o in ((ArrayList)result[result["result"]])) {
+					foreach (object key in ((Hashtable)o).Keys) {
+						Console.WriteLine(key + ":" + ((Hashtable)o)[key]);
+					}
 				}
 			}
+
 			Console.ReadLine();
 		}
 	}
