1e2637668fe5eb32452d4f35753c82ac7d437a8e
Alec Gorge <alecgorge@gmail.com>
2011-06-11 16:58:38 -0400

    CB 860 compile, FINALLY fix streams

--- src/com/ramblingwood/minecraft/jsonapi/JSONAPI.java
+++ src/com/ramblingwood/minecraft/jsonapi/JSONAPI.java
@@ -279,6 +279,7 @@ public class JSONAPI extends JavaPlugin implements RTKListener {
 			// add console stream support
 			handler = new ConsoleHandler(jsonServer);
 			log.addHandler(handler);
+			outLog.addHandler(handler);
 
 			jsonSocketServer = new JSONSocketServer(port + 1, jsonServer);
 			jsonWebSocketServer = new JSONWebSocketServer(port + 2, jsonServer);
