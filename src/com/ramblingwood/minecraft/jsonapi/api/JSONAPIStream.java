a5605d9fda991843aeffa242c80b8705bb18c8c3
Alec Gorge <alecgorge@gmail.com>
2011-07-10 12:05:11 -0400

    docs

--- src/com/ramblingwood/minecraft/jsonapi/api/JSONAPIStream.java
+++ src/com/ramblingwood/minecraft/jsonapi/api/JSONAPIStream.java
@@ -7,11 +7,22 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import com.ramblingwood.minecraft.jsonapi.JSONAPI;
 
+/**
+ * This class represents a stream. You most likely never have to overload any of these methods.
+ * 
+ * @author alecgorge
+ */
 public abstract class JSONAPIStream {
 	protected List<JSONAPIStreamListener> listeners = Collections.synchronizedList(new ArrayList<JSONAPIStreamListener>());
 	protected LinkedBlockingQueue<JSONAPIStreamMessage> messages = new LinkedBlockingQueue<JSONAPIStreamMessage>();
 	protected List<JSONAPIStreamMessage> last50 = Collections.synchronizedList(new ArrayList<JSONAPIStreamMessage>(50));
 	
+	/**
+	 * Register to have messages pushed to the listener.
+	 * 
+	 * @param l The listener to push to.
+	 * @param feedOld Do you want to receive up to 50 previous messages immediately after registering?
+	 */
 	public void registerListener(JSONAPIStreamListener l, boolean feedOld) {
 		if(feedOld) {
 			for(JSONAPIStreamMessage m : last50) {
@@ -22,18 +33,39 @@ public abstract class JSONAPIStream {
 		listeners.add(l);
 	}
 	
+	/**
+	 * Returns up to the last 50 messages in syncronized ArrayList.
+	 * 
+	 * @return
+	 */
 	public List<JSONAPIStreamMessage> getStack() {
 		return last50;
 	}
 	
+	
+	/**
+	 * Undo the effects of registerListener.
+	 * 
+	 * @param l
+	 */
 	public void deregisterListener(JSONAPIStreamListener l) {
 		listeners.remove(l);
 	}
 	
+	/**
+	 * Alias for pushMessage
+	 * 
+	 * @param m
+	 */
 	public void addMessage(JSONAPIStreamMessage m) {
 		pushMessage(m);
 	}
 	
+	/**
+	 * Push out a message to all subscribers.
+	 * 
+	 * @param m A message to be pushed to all subscribers.
+	 */
 	public void pushMessage(final JSONAPIStreamMessage m) {
 		final JSONAPIStream that = this;
 		JSONAPI.instance.getServer().getScheduler().scheduleSyncDelayedTask(JSONAPI.instance, new Runnable() {
--- src/com/ramblingwood/minecraft/jsonapi/api/JSONAPIStreamMessage.java
+++ src/com/ramblingwood/minecraft/jsonapi/api/JSONAPIStreamMessage.java
@@ -5,15 +5,32 @@ import java.util.Calendar;
 import org.json.simpleForBukkit.JSONAware;
 import org.json.simpleForBukkit.JSONObject;
 
+/**
+ * This class will need to be subcalled to provide meaningful data. By default, there are just some convenience methods.
+ * 
+ * It is recommended that you call "setTime()" in your constructor to make sure the timestamp is as accurate as possible.
+ * 
+ * @author alecgorge
+ */
 abstract public class JSONAPIStreamMessage implements JSONAware {
 	protected long time = -1;
 
+	/**
+	 * This return value of this method should be the same as the name of the stream when register it with JSONAPI.
+	 * 
+	 * @return the stream name
+	 */
 	public abstract String streamName();
 	
 	public void setTime(long time) {
 		this.time = time;
 	}
-	
+
+	/**
+	 * If setTime was previously called, returns the value setTime set. Otherwise, calls setTime and returns the value. 
+	 * 
+	 * @return The timestamp store.
+	 */
 	public long getTime() {
 		if(time == -1) {
 			setTime();
@@ -21,6 +38,9 @@ abstract public class JSONAPIStreamMessage implements JSONAware {
 		return time;
 	}
 
+	/**
+	 * Takes a timestamp of the current time and stores it for usage later when the JSONObject is being created.
+	 */
 	public void setTime() {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.MILLISECOND, -cal.get(Calendar.DST_OFFSET) - cal.get(Calendar.ZONE_OFFSET));
@@ -29,6 +49,20 @@ abstract public class JSONAPIStreamMessage implements JSONAware {
 		time = lUTCtime/1000;
 	}
 
+	/**
+	 * Returns an org.json.simpleForBukkit.JSONObject that is a JSON representation of the data in this class.
+	 * 
+	 * Example:
+	 * 
+	 * <code>
+	 * JSONObject o = new JSONObject();
+	 * o.put("time", getTime());
+	 * o.put("player", getPlayer());
+	 * o.put("message", getMessage());
+	 * return o;
+	 * </code>
+	 * @return
+	 */
 	public abstract JSONObject toJSONObject ();
 	/*
 		Example:
