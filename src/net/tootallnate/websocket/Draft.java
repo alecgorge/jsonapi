057778d54c5bba4c242ecf37e9077c25a4756503
Alec Gorge <alecgorge@gmail.com>
2012-01-08 18:10:02 -0500

    update upstream websocket deps. hopefully close #90

--- src/net/tootallnate/websocket/Draft.java
+++ src/net/tootallnate/websocket/Draft.java
@@ -1,22 +1,15 @@
 package net.tootallnate.websocket;
 
 import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import net.tootallnate.websocket.WebSocket.Role;
 import net.tootallnate.websocket.exeptions.InvalidHandshakeException;
 
 public abstract class Draft{
-	
-	/**
-	* The WebSocket protocol expects UTF-8 encoded bytes.
-	*/ 
-	public static final  Charset  UTF8_CHARSET = Charset.forName ( "UTF-8" );
-	private static final byte[] FLASH_POLICY_REQUEST = "<policy-file-request/>".getBytes( UTF8_CHARSET );
 	
 	public enum HandshakeState{ 
 		/**Handshake matched this Draft successfully*/
@@ -28,51 +21,28 @@ public abstract class Draft{
 		//,/**Can not yet say anything*/
 		//PENDING  not yet in use
 	}
+	/**
+	* The WebSocket protocol expects UTF-8 encoded bytes.
+	*/ 
+	private static final byte[] FLASH_POLICY_REQUEST = WebSocket.utf8Bytes("<policy-file-request/>");
 	
-	public abstract HandshakeState acceptHandshakeAsServer( Handshakedata handshakedata ) throws InvalidHandshakeException;
-	public abstract HandshakeState acceptHandshakeAsClient( Handshakedata request , Handshakedata response ) throws InvalidHandshakeException;
-	public abstract List<Framedata> translateFrame( ByteBuffer buffer, int read );
-	public abstract ByteBuffer createBinaryFrame( Framedata framedata ); //TODO Allow to send data on the base of an Iterator or InputStream 
-	public abstract List<Framedata> createFrames(  String text , boolean mask );
-	public abstract List<Framedata> createFrames(  byte[] binary , boolean mask );
-	public abstract HandshakeBuilder postProcessHandshakeRequestAsClient( HandshakeBuilder request ) throws InvalidHandshakeException;
-	public abstract HandshakeBuilder postProcessHandshakeResponseAsServer(  Handshakedata request , HandshakeBuilder response ) throws InvalidHandshakeException;
-	
-	public List<ByteBuffer> createHandshake( Handshakedata handshakedata , Role ownrole ){
-		return createHandshake( handshakedata , ownrole , true );
+	/**will return the index of the first \r\n or the index off the last element in arr*/
+	public static int findNewLine( byte[] arr , int offset ) {
+		int len = arr.length - 1;
+		int i = offset;
+		  for (  ; i < len ; i++ ) 
+			if( arr[i] == (byte)'\r' && arr[ i + 1 ] == (byte)'\n' )
+				return i;
+		return i;//the end of input will be handled like newline
 	}
 	
-	public List<ByteBuffer> createHandshake( Handshakedata handshakedata , Role ownrole , boolean withcontent ){
-		StringBuilder bui = new StringBuilder ( 100 );
-		if( ownrole == Role.CLIENT){
-			bui.append ( "GET " );
-			bui.append ( handshakedata.getResourceDescriptor () );
-			bui.append ( " HTTP/1.1" );
+	public static boolean isFlashEdgeCase( byte[] request , int requestsize ){
+		for( int i = 0 ; i < requestsize && i < FLASH_POLICY_REQUEST.length ; i++ ){
+			if( FLASH_POLICY_REQUEST[i] != request[i] ){
+				return false;
+			}
 		}
-		else if( ownrole == Role.SERVER ){
-			bui.append ( "HTTP/1.1 101 "+handshakedata.getHttpStatusMessage() );
-		}
-		else{
-			throw new RuntimeException ( "unknow role" );
-		}
-		bui.append ( "\r\n" );
-		Iterator<String> it = handshakedata.iterateHttpFields ();
-		while( it.hasNext () ){
-			String fieldname = it.next ();
-			String fieldvalue = handshakedata.getFieldValue ( fieldname );
-			bui.append ( fieldname );
-			bui.append ( ": ");
-			bui.append ( fieldvalue );
-			bui.append ( "\r\n" );
-		}
-		bui.append ( "\r\n" );
-		byte[] httpheader = bui.toString ().getBytes ( UTF8_CHARSET );
-		byte[] content = withcontent ? handshakedata.getContent() : null;
-		ByteBuffer bytebuffer = ByteBuffer.allocate ( ( content==null ? 0 : content.length ) + httpheader.length );
-		bytebuffer.put ( httpheader );
-		if( content!=null )
-			bytebuffer.put ( content );
-		return Collections.singletonList( bytebuffer );
+		return requestsize >= FLASH_POLICY_REQUEST.length;
 	}
 	
 	public static HandshakeBuilder translateHandshakeHttp( byte[] buffer, int readcount ) throws InvalidHandshakeException{
@@ -115,27 +85,66 @@ public abstract class Draft{
 		return draft;
 	}
 	
-	public Handshakedata translateHandshake( byte[] buffer, int readcount ) throws InvalidHandshakeException{
-		return translateHandshakeHttp( buffer , readcount );
-	}
-
-	/**will return the index of the first \r\n or the index off the last element in arr*/
-	public static int findNewLine( byte[] arr , int offset ) {
-		int len = arr.length - 1;
-		int i = offset;
-		  for (  ; i < len ; i++ ) 
-			if( arr[i] == (byte)'\r' && arr[ i + 1 ] == (byte)'\n' )
-				return i;
-		return i;//the end of input will be handled like newline
+	public abstract HandshakeState acceptHandshakeAsClient( Handshakedata request , Handshakedata response ) throws InvalidHandshakeException;
+	
+	public abstract HandshakeState acceptHandshakeAsServer( Handshakedata handshakedata ) throws InvalidHandshakeException;
+	
+	protected boolean basicAccept( Handshakedata handshakedata ){
+		return handshakedata.getFieldValue( "Upgrade" ).equalsIgnoreCase( "websocket" )
+			&& handshakedata.getFieldValue( "Connection" ).toLowerCase( Locale.ENGLISH ).contains( "upgrade" );
 	}
 	
-	public static boolean isFlashEdgeCase( byte[] request , int requestsize ){
-		for( int i = 0 ; i < requestsize && i < FLASH_POLICY_REQUEST.length ; i++ ){
-			if( FLASH_POLICY_REQUEST[i] != request[i] ){
-				return false;
-			}
+	public abstract ByteBuffer createBinaryFrame( Framedata framedata ); //TODO Allow to send data on the base of an Iterator or InputStream 
+	
+	public abstract List<Framedata> createFrames(  byte[] binary , boolean mask );
+	
+	public abstract List<Framedata> createFrames(  String text , boolean mask );
+	
+	public List<ByteBuffer> createHandshake( Handshakedata handshakedata , Role ownrole ){
+		return createHandshake( handshakedata , ownrole , true );
+	}
+	
+	public List<ByteBuffer> createHandshake( Handshakedata handshakedata , Role ownrole , boolean withcontent ){
+		StringBuilder bui = new StringBuilder ( 100 );
+		if( ownrole == Role.CLIENT){
+			bui.append ( "GET " );
+			bui.append ( handshakedata.getResourceDescriptor () );
+			bui.append ( " HTTP/1.1" );
 		}
-		return requestsize >= FLASH_POLICY_REQUEST.length;
+		else if( ownrole == Role.SERVER ){
+			bui.append ( "HTTP/1.1 101 "+handshakedata.getHttpStatusMessage() );
+		}
+		else{
+			throw new RuntimeException ( "unknow role" );
+		}
+		bui.append ( "\r\n" );
+		Iterator<String> it = handshakedata.iterateHttpFields ();
+		while( it.hasNext () ){
+			String fieldname = it.next ();
+			String fieldvalue = handshakedata.getFieldValue ( fieldname );
+			bui.append ( fieldname );
+			bui.append ( ": ");
+			bui.append ( fieldvalue );
+			bui.append ( "\r\n" );
+		}
+		bui.append ( "\r\n" );
+		byte[] httpheader = WebSocket.utf8Bytes( bui.toString ());
+		byte[] content = withcontent ? handshakedata.getContent() : null;
+		ByteBuffer bytebuffer = ByteBuffer.allocate ( ( content==null ? 0 : content.length ) + httpheader.length );
+		bytebuffer.put ( httpheader );
+		if( content!=null )
+			bytebuffer.put ( content );
+		return Collections.singletonList( bytebuffer );
+	}
+	
+	public abstract HandshakeBuilder postProcessHandshakeRequestAsClient( HandshakeBuilder request ) throws InvalidHandshakeException;
+	
+	public abstract HandshakeBuilder postProcessHandshakeResponseAsServer(  Handshakedata request , HandshakeBuilder response ) throws InvalidHandshakeException;
+
+	public abstract List<Framedata> translateFrame( ByteBuffer buffer, int read );
+	
+	public Handshakedata translateHandshake( byte[] buffer, int readcount ) throws InvalidHandshakeException{
+		return translateHandshakeHttp( buffer , readcount );
 	}
 
 }
