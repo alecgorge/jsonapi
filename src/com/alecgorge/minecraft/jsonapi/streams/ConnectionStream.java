d4c9a9a27d6214836e98ad24b07bd74da650091a
Alec Gorge <alecgorge@gmail.com>
2012-05-18 10:25:50 -0400

    initial untested support for pushed streams (max every 10 seconds)

--- src/com/alecgorge/minecraft/jsonapi/streams/ConnectionStream.java
+++ src/com/alecgorge/minecraft/jsonapi/streams/ConnectionStream.java
@@ -3,4 +3,7 @@ package com.alecgorge.minecraft.jsonapi.streams;
 import com.alecgorge.minecraft.jsonapi.api.JSONAPIStream;
 
 public class ConnectionStream extends JSONAPIStream {
+	public ConnectionStream(String name) {
+		super(name);
+	}
 }
