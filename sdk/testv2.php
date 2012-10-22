f9550992bac08ac83eca100b3e308da0d165b173
Alec Gorge <alecgorge@gmail.com>
2012-10-22 17:36:16 -0400

    php v2 test

---
+++ sdk/testv2.php
@@ -0,0 +1,84 @@
+#!/usr/bin/env php
+<?php
+
+$host = "localhost";
+$port = 20059;
+$endpoint = "api/2/call";
+
+$username = "usernameGoesHere";
+$password = "passwordGoesHere";
+$salt = "salt goes here";
+
+function gen_key($name) {
+	global $username, $password, $salt;
+	return hash('sha256', $username . $name . $password . $salt);
+}
+
+
+$methodName = "getPlayers";
+
+$payload = array(
+	array(
+		'name' => $methodName,
+		'key' => gen_key($methodName),
+		'username' => $username,
+	),
+	array(
+		'name' => 'getWorlds',
+		'key' => gen_key('getWorlds'),
+		'username' => $username,
+	),
+	array(
+		'name' => 'system.getServerClockDebug',
+		'key' => gen_key('system.getServerClockDebug'),
+		'username' => $username,
+	),
+	array(
+		'name' => 'system.getJavaMemoryUsage',
+		'key' => gen_key('system.getJavaMemoryUsage'),
+		'username' => $username,
+	),
+	array(
+		'name' => 'system.getJavaMemoryTotal',
+		'key' => gen_key('system.getJavaMemoryTotal'),
+		'username' => $username,
+	),
+	array(
+		'name' => 'system.getDiskUsage',
+		'key' => gen_key('system.getDiskUsage'),
+		'username' => $username,
+	),
+	array(
+		'name' => 'system.getDiskSize',
+		'key' => gen_key('system.getDiskSize'),
+		'username' => $username,
+	)
+);
+
+if(!(php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR']))) {
+	echo "<pre>";
+}
+
+$url = sprintf("http://%s:%d/%s?json=%s", $host, $port, $endpoint, rawurlencode(json_encode($payload)));
+echo $url ."\n\n\n";
+
+$c = curl_init($url);
+curl_setopt($c, CURLOPT_PORT, $port);
+curl_setopt($c, CURLOPT_HEADER, true);
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($c, CURLOPT_TIMEOUT, 10);		
+
+curl_setopt($c, CURLOPT_VERBOSE, 0);
+
+$response = curl_exec($c);
+
+// Then, after your curl_exec call:
+$header_size = curl_getinfo($c, CURLINFO_HEADER_SIZE);
+$header = substr($response, 0, $header_size);
+$body = json_decode(substr($response, $header_size), true);
+
+curl_close($c);
+
+echo $header;
+
+print_r($body);
