32c274d5df63ae1a2aaacdc4ffab63044dff6661
Alec Gorge <alecgorge@gmail.com>
2014-03-28 16:44:53 -0400

    add missing file

---
+++ src/main/java/com/alecgorge/minecraft/jsonapi/packets/netty/JSONAPINettyInjector.java
@@ -0,0 +1,35 @@
+package com.alecgorge.minecraft.jsonapi.packets.netty;
+
+//#if mc17OrNewer=="yes"
+import net.minecraft.util.io.netty.channel.Channel;
+
+import com.alecgorge.minecraft.jsonapi.packets.netty.router.JSONAPIDefaultRoutes;
+//#endif
+
+import com.alecgorge.minecraft.jsonapi.JSONAPI;
+
+public class JSONAPINettyInjector {
+	//#if mc17OrNewer=="yes"
+	NettyInjector injector = null;
+	//#endif
+	
+	public JSONAPINettyInjector(final JSONAPI api) {
+		//#if mc17OrNewer=="yes"
+		injector = new NettyInjector() {
+	        @Override
+	        protected void injectChannel(Channel channel) {
+	            channel.pipeline().addFirst(new JSONAPIChannelDecoder(api));
+	        }
+	    };
+	    injector.inject();
+	    
+	    new JSONAPIDefaultRoutes(api);
+	    //#endif
+	}
+	
+	public void close() {
+		//#if mc17OrNewer=="yes"
+		injector.close();
+		//#endif
+	}
+}
